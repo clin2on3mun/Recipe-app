e14252d1303742b2efbbff913bc2868bffc3764b
clin2on3mun <munanaclinton@gmail.com>
2023-10-10 07:27:05 +0200

    Add login and Registration actions functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   root "public_recipes#index"
-
+  
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+  
   resources :foods, only: [:index, :create, :new, :show, :destory]
   resources :recipes, only: [:index, :create, :show, :destory] do 
     resources :recipe_foods, only: [:new, :create, :destory]
