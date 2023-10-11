7634c7eb0cf4e0fb1af903e5c6b42e62ac995d94
moFuhidy <mo7ammed9290@gmail.com>
2023-10-11 16:55:42 +0300

    Add recipe_food tests

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+require 'rspec/rails'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recipe) { Recipe.create(name: 'Kabsah') }
+  let(:food) { Food.create(name: 'Rice', price: 10.0) }
+
+  subject { described_class.new(quantity: 5, recipe: recipe, food: food) }
+
+  describe 'validations' do
+    it 'Is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'Is not valid without a quantity' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid with a non-positive quantity' do
+      subject.quantity = 0
+      expect(subject).to_not be_valid
+
+      subject.quantity = -1
+      expect(subject).to_not be_valid
+    end
+  end
 end
