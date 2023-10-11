e8d03f8035b48efc37da263e489a45fb2e00a9a0
clin2on3mun <munanaclinton@gmail.com>
2023-10-11 14:53:16 +0200

    Add changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods
-
+  has_many :recipe_foods, through: :recipes
   validates :name, presence: true
 end
