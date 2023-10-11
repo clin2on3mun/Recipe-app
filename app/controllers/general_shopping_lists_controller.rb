1db8d02c959e1f67f3a622e3facdc76a6cfc9ee7
clin2on3mun <munanaclinton@gmail.com>
2023-10-11 15:08:46 +0200

    Fix linters

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,7 +1,6 @@
 class GeneralShoppingListsController < ApplicationController
-    def index
-        @recipe_foods = current_user.recipe_foods.group(:food_id).sum(:quantity)
-        @foods = current_user.foods    
-    end
+  def index
+    @recipe_foods = current_user.recipe_foods.group(:food_id).sum(:quantity)
+    @foods = current_user.foods
+  end
 end
-
