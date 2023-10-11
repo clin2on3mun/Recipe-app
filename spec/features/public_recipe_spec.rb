412d0a7e12c5b20471abdfd74ad4fbfc2a701a44
clin2on3mun <munanaclinton@gmail.com>
2023-10-11 21:29:37 +0200

    Fix linter errors

--- spec/features/public_recipe_spec.rb
+++ spec/features/public_recipe_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe 'PubicRecipe specs', type: :feature do
   before(:each) do
     Recipe.delete_all
 
-    @user = User.create!(name: 'becky', email: 'munana@gmail.com', password: 'abcxyz123', password_confirmation: 'abcxyz123', confirmed_at: Time.now)    
-    @recipe1 = Recipe.create(user: @user, name: 'Kabsah', description: 'The best in the west', cooking_time: 45, preparation_time: 40, public: true)
-    visit new_user_session_path  # Assuming this is your sign-in page
+    @user = User.create!(name: 'becky', email: 'munana@gmail.com', password: 'abcxyz123',
+                         password_confirmation: 'abcxyz123', confirmed_at: Time.now)
+    @recipe1 = Recipe.create(user: @user, name: 'Kabsah', description: 'The best in the west', cooking_time: 45,
+                             preparation_time: 40, public: true)
+    visit new_user_session_path # Assuming this is your sign-in page
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
     click_button 'Log in'
     visit public_recipes_path
-    
   end
-  
+
   it 'see the title' do
-    expect(page).to have_content("Public Recipes")
+    expect(page).to have_content('Public Recipes')
   end
   it 'see the card' do
-    expect(page).to have_content("Kabsah")
+    expect(page).to have_content('Kabsah')
   end
   it 'see name of recipe owner' do
-    expect(page).to have_content("becky")
+    expect(page).to have_content('becky')
   end
 end
-
