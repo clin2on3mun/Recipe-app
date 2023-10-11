e33e6a0090526abcbb6d7147eb1f5ee66c218b1d
clin2on3mun <munanaclinton@gmail.com>
2023-10-11 14:50:48 +0200

    Add general shopping controller and its views

---
+++ app/controllers/general_shopping_lists_controller.rb
@@ -0,0 +1,7 @@
+class GeneralShoppingListsController < ApplicationController
+    def index
+        @recipe_foods = current_user.recipe_foods.group(:food_id).sum(:quantity)
+        @foods = current_user.foods    
+    end
+end
+
